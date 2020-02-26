dce3f12a51c6f9a715230ee286c2ce34489bf180
KyungMinJin <kyung6479@korea.ac.kr>
2020-02-26 22:08:34 +0900

    로컬 or 서버 port 설정

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import Express from 'express';
 
 const app = Express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT ? process.env.PORT : 3000;
 
 app.get('/', (req, res) => {
   res.send('index page');
