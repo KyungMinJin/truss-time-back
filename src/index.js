ca112128560b09da94ad4f38ffc45fd1508ada3e
kyungminjin <kyung6479@korea.ac.kr>
2020-03-25 23:25:12 +0900

    날리기 전 커밋

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT ? process.env.PORT : 3000;
 dbconfig();
 // Routing 설정
 app.use('/api', api);
-
+//날리고 원래 꺼 덮을게요
 app.listen(PORT, () => {
   console.log(`${PORT} port에 http server를 띄웠습니다.`);
 });
