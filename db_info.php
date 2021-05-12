f3c9e429ca908ffb72488b7cec7db6fc9585fb4f
진경민 <kyung6479@korea.ac.kr>
2021-05-13 00:09:17 +0900

    db 변경

--- db_info.php
+++ db_info.php
@@ -4,14 +4,14 @@
 //$mysql_database = "u221597977_table";
 //$mysql_user = "u221597977_truss";
 //$mysql_password = "voy091011";
-$mysql_host = "us-cdbr-iron-east-03.cleardb.net";
-$mysql_database = "heroku_86026cc2c546d7c";
-$mysql_user = "b961c4cc84e119";
-$mysql_password = "d155f17e";
-// $mysql_host = "us-cdbr-iron-east-04.cleardb.net";
-// $mysql_database = "heroku_e1b46640afdb0f1";
-// $mysql_user = "b36fc57ade02f1";
-// $mysql_password = "8489c862";
+// $mysql_host = "us-cdbr-iron-east-03.cleardb.net";
+// $mysql_database = "heroku_86026cc2c546d7c";
+// $mysql_user = "b961c4cc84e119";
+// $mysql_password = "d155f17e";
+$mysql_host = "us-cdbr-iron-east-04.cleardb.net";
+$mysql_database = "heroku_e1b46640afdb0f1";
+$mysql_user = "b36fc57ade02f1";
+$mysql_password = "8489c862";
 
 $connect = mysqli_connect("$mysql_host", "$mysql_user", "$mysql_password", "$mysql_database");
 mysqli_query($connect, "SET NAMES utf8");
