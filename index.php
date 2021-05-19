a76adbe24e4344bd8df0a3c39c49abae6040dc01
진경민 <kyung6479@korea.ac.kr>
2021-05-20 02:46:40 +0900

    footer

--- index.php
+++ index.php
@@ -246,8 +246,8 @@ Released   : 20130602
                 rel="nofollow">TEMPLATED</a></p>
         <ul class="contact">
             <!--<li><a href="#" class="icon icon-twitter"><span>Twitter</span></a></li>-->
-            <li><a href="https://www.facebook.com/profile.php?id=100009047977821"
-                    class="icon icon-facebook"><span></span></a></li>
+            <!-- <li><a href="https://www.facebook.com/profile.php?id=100009047977821"
+                    class="icon icon-facebook"><span></span></a></li> -->
             <!--<li><a href="#" class="icon icon-dribbble"><span>Pinterest</span></a></li>
 			<li><a href="#" class="icon icon-tumblr"><span>Google+</span></a></li>
 			<li><a href="#" class="icon icon-rss"><span>Pinterest</span></a></li>
